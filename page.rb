7dc60b96c9d77ea4629c826bd2e405ac3ea4ef44
jugyo <jugyo.org@gmail.com>
2009-05-06 23:27:11 +0900

    Created model for page

--- page.rb
+++ page.rb
@@ -1,30 +1,68 @@
+require 'rubygems'
+require 'git_store'
+
 module Gitki
-  def self.setup(git_store_path)
+  def self.setup(git_store_path, wiki_dir = 'wiki')
     @@store = GitStore.new(git_store_path)
+    @@wiki_dir = wiki_dir
   end
 
   def store
     @@store
   end
 
+  def wiki_dir
+    @@wiki_dir
+  end
+
   def create_default_pages
-    create_home unless page('home')
-    create_navigation unless page('navigation')
+    create_home unless Page.find('home')
+    create_navigation unless Page.find('navigation')
   end
 
   def create_home
-    template = open(File.dirname(__FILE__) + '/home_template.haml').read
-    store[store_path('home')] = {:title => 'Home', :body => template}
-    store.commit 'Create as defaut'
+    Page.create('home', 'Home', read_template('home_template.haml'))
   end
 
   def create_navigation
-    template = open(File.dirname(__FILE__) + '/navigation_template.haml').read
-    store[store_path('navigation')] = {:title => 'Navigation', :body => template}
-    store.commit 'Create as defaut'
+    Page.create('navigation', 'Navigation', read_template('navigation_template.haml'))
+  end
+
+  def read_template(name)
+    open(File.dirname(__FILE__) + '/' + name).read
   end
 
   class Page
-    
+    class << self
+      def find_all
+        pages = {}
+        store[wiki_dir].to_hash.each do |name, text|
+          pages[name] = split_title_and_body(text)
+        end
+        pages
+      end
+
+      def find(name)
+        split_title_and_body(store[store_path(name)])
+      end
+
+      def create(name, title, body)
+        store[store_path(name)] = "#{title}\n\n#{body}"
+        store.commit
+      end
+
+      def store_path(name)
+        File.join(wiki_dir, name)
+      end
+
+      def split_title_and_body(text)
+        return nil if text.nil? || text.empty?
+        lines = text.split("\n")
+        title = lines.shift
+        lines.shift
+        body = lines.join("\n")
+        {:title => title, :body => body}
+      end
+    end
   end
 end
---
+++ spec/page_spec.rb
@@ -0,0 +1,56 @@
+$: << File.dirname(__FILE__) + '/../'
+require 'page'
+require 'tmpdir'
+require 'ruby-debug'
+
+include Gitki
+
+describe Page do
+  before do
+    dir = Dir.tmpdir
+    Dir.chdir(dir) do |path|
+      system 'rm', '-rf', '.git'
+      system 'git', 'init'
+    end
+    Gitki.setup(dir)
+  end
+
+  describe 'when create_default_pages' do
+    before(:each) do
+      create_default_pages
+    end
+
+    it 'should return 2 pages' do
+      pages = Page.find_all
+      pages.size.should == 2
+      
+      page = Page.find('home')
+      page[:title].should == "Home"
+      page[:body].should == read_template('home_template.haml').sub(/\n+\Z/m, '')
+
+      page = Page.find('navigation')
+      page[:title].should == "Navigation"
+      page[:body].should == read_template('navigation_template.haml').sub(/\n+\Z/m, '')
+    end
+  end
+
+  describe 'create 2 page' do
+    before(:each) do
+      Page.create('foo', 'foo', 'foo foo foo')
+      Page.create('bar', 'bar', 'bar bar bar')
+    end
+
+    it 'should return 2 pages' do
+      pages = Page.find_all
+      pages.size.should == 2
+
+      page = pages['foo']
+      page[:title] = 'foo'
+      page[:body] = 'foo foo foo'
+
+      page = pages['bar']
+      page[:title] = 'bar'
+      page[:body] = 'bar bar bar'
+    end
+  end
+end
