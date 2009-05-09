6b5357e9ba6cb6f433a0f1a169ade74a66657ca0
jugyo <jugyo.org@gmail.com>
2009-05-10 00:09:52 +0900

    refactoring

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -9,17 +9,23 @@ include Gitki
 describe Gitki do
   REPO = File.join Dir.tmpdir, 'git_store_test'
 
-  before do
+  before(:each) do
     FileUtils.rm_rf REPO
     Dir.mkdir REPO
     Dir.chdir(REPO) do |path|
       system 'git', 'init', '--bare'
     end
-    Gitki.setup(REPO)
   end
 
-  describe Page do
+  describe 'repository is empty' do
+    it 'should create defaults' do
+      Gitki.should_receive(:create_defaults)
+      Gitki.setup(REPO)
+    end
+
     it 'should return 2 pages' do
+      Gitki.setup(REPO)
+
       pages = Page.find_all
       pages.size.should == 2
     
@@ -30,6 +36,26 @@ describe Gitki do
       page = Page.find('navigation')
       page[:title].should == "Navigation"
       page[:body].should == Gitki.read_template('navigation_template.haml').sub(/\n+\Z/m, '')
+    end
+  end
+  
+
+  describe 'repository is not empty' do
+    before(:each) do
+      store = GitStore.new(REPO, 'master', true)
+      store['test.txt'] = 'test'
+      store.commit 'Added test.txt'
+    end
+
+    it 'should not create defaults' do
+      Gitki.should_not_receive(:create_defaults)
+      Gitki.setup(REPO)
+    end
+  end
+
+  describe Page do
+    before(:each) do
+      Gitki.setup(REPO)
     end
 
     describe 'create 2 page' do
@@ -55,6 +81,7 @@ describe Gitki do
 
   describe Attachment do
     before(:each) do
+      Gitki.setup(REPO)
       Gitki.store['files/test.txt'] = 'test'
     end
 
